e9304932b885e75a57fd5a3890fd07c3913211b1
Eric van Zanten <eric.vanzanten@gmail.com>
2016-09-13 16:29:01 -0500

    Add validate service.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# Make directory for project. This may already exist but it's a good practice
-# to make sure this pipeline will work on a bare server.
+# Make project directory if it doesn't exist. This is mainly to ensure that these scripts work on a bare server
 mkdir -p /home/datamade/nmid
+
+# Decrypt files encrypted with blackbox
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && sudo -H -u datamade blackbox_postdeploy
