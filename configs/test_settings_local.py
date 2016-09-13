2d9690411dcbf7c8ea6014776af4023c4996496a
Eric van Zanten <eric.vanzanten@gmail.com>
2016-09-13 16:45:14 -0500

    Adding Travis config

---
+++ configs/test_settings_local.py
@@ -0,0 +1,21 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'super secret key'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nmid',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
