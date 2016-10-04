ceda0a0867783857316a156357a2c7f3d385f4c2
Eric van Zanten <eric.vanzanten@gmail.com>
2016-10-04 12:11:08 -0500

    Tweak 500 template. Add sentry stuff

--- nmid/settings.py
+++ nmid/settings.py
@@ -16,7 +16,6 @@ from .settings_local import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -36,6 +35,7 @@ INSTALLED_APPS = [
     'camp_fin.apps.CampFinConfig',
     'pages.apps.PagesConfig',
     'ckeditor',
+    'raven.contrib.django.raven_compat',
 ]
 
 MIDDLEWARE = [
