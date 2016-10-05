bcf38889727f55f0700a316a65c99b71989a80db
Eric van Zanten <eric.vanzanten@gmail.com>
2016-10-05 13:44:16 -0500

    Trying to get some basic tests going

--- camp_fin/tests.py
+++ camp_fin/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django.urls import reverse
 
-# Create your tests here.
+TEST_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_nmid',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+@override_settings(DATABASES=TEST_DATABASES)
+class TestViews(TestCase):
+    def test_index(self):
+        response = self.client.get(reverse('index'))
+        assert response.status_code == 200
+
+    def test_about(self):
+        response = self.client.get(reverse('about'))
+        assert response.status_code == 200
+
+    def test_donations(self):
+        response = self.client.get(reverse('donations'))
+        assert response.status_code == 200
