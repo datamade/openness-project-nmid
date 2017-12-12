3145b9e2732e530d428b2796b7492ecc107729ef
Jean Cochrane <jean@jeancochrane.com>
2017-12-12 17:19:14 -0600

    Require django-pdb for testing

--- nmid/settings.py
+++ nmid/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Application definition
 
 INSTALLED_APPS = [
+    'django_pdb',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
