b4e776dafa6ed61d057475d74c30ac7d95730de3
Hannah Cushman Garland <hannah.cushman@datamade.us>
2023-11-06 09:47:13 -0600

    Configure locales in container

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,13 @@ FROM python:3.6
 LABEL maintainer "DataMade <info@datamade.us>"
 
 RUN apt-get update && \
-	apt-get install -y --no-install-recommends postgresql-client
+	apt-get install -y --no-install-recommends postgresql-client locales && \
+	rm -rf /var/lib/apt/lists/* && \
+	sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+    dpkg-reconfigure --frontend=noninteractive locales
+
+ENV LANG en_US.UTF-8
+ENV LC_NUMERIC en_US.UTF-8
 
 RUN mkdir /app
 WORKDIR /app
@@ -14,6 +20,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . /app
 
 ENV DJANGO_SECRET_KEY 'foobar'
+ENV DJANGO_FLUSH_CACHE_KEY 'foobar'
 ENV DJANGO_DEBUG 'False'
 
 RUN python manage.py collectstatic --noinput
--- camp_fin/templatetags/helpers.py
+++ camp_fin/templatetags/helpers.py
@@ -2,62 +2,70 @@ from django.template import Library
 
 register = Library()
 
+
 @register.filter
 def clean_county(county):
-    if county.name == 'ALL':
-        return ''
+    if county.name == "ALL":
+        return ""
     else:
         return county
+
 
 @register.filter()
 def format_money(s):
     import locale
-    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+    locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
     if s:
         s = float(s)
         return locale.currency(s, grouping=True)
-    return '$0.00'
+    return "$0.00"
+
 
 @register.filter()
 def format_money_short(n):
     if not n:
-        return '$0'
+        return "$0"
     import math
-    millnames=['','K','M','B']
+
+    millnames = ["", "K", "M", "B"]
     n = float(n)
-    millidx=max(0,min(len(millnames)-1,
-                      int(math.floor(math.log10(abs(n))/3))))
-    return '$%.1f%s'%(n/10**(3*millidx),millnames[millidx])
+    millidx = max(0, min(len(millnames) - 1, int(math.floor(math.log10(abs(n)) / 3))))
+    return "$%.1f%s" % (n / 10 ** (3 * millidx), millnames[millidx])
+
 
 @register.simple_tag
 def query_transform(request, **kwargs):
     updated = request.GET.copy()
-    for k,v in kwargs.items():
+    for k, v in kwargs.items():
         updated[k] = v
     return updated.urlencode()
 
+
 @register.filter
 def get_sort_icon(s):
-    if s.lower() == 'desc':
+    if s.lower() == "desc":
         return ' <i class="fa fa-sort-amount-desc"> </i>'
     return ' <i class="fa fa-sort-amount-asc"> </i>'
+
 
 @register.filter
 def get_transaction_verb(s):
     verbs = {
-        'Monetary contribution': 'donated',
-        'In Kind contribution': 'donated in-kind',
-        'Anonymous Contribution': 'anonymously donated',
-        'Refund monetary  (NOT BEING USED)': 'was refunded',
-        'Monetary Expenditure': 'spent',
+        "Monetary contribution": "donated",
+        "In Kind contribution": "donated in-kind",
+        "Anonymous Contribution": "anonymously donated",
+        "Refund monetary  (NOT BEING USED)": "was refunded",
+        "Monetary Expenditure": "spent",
     }
-    return verbs.get(s, '')
+    return verbs.get(s, "")
+
 
 @register.simple_tag
 def funds_raised(campaign, year, short=False):
-    '''
+    """
     Retrives the amount of funds raised for a candidate in a given year.
-    '''
+    """
     funds = campaign.funds_raised(since=year)
 
     if short:
@@ -67,11 +75,12 @@ def funds_raised(campaign, year, short=False):
 
     return output
 
+
 @register.simple_tag
 def expenditures(campaign, year, short=False):
-    '''
+    """
     Retrives the expenditures for a candidate in a given year.
-    '''
+    """
     expenditures = campaign.expenditures(since=year)
 
     if short:
@@ -81,33 +90,37 @@ def expenditures(campaign, year, short=False):
 
     return output
 
+
 @register.filter
 def largest_contribution(races):
-    '''
+    """
     Return the largest amount of funds raised in single race among a set of races.
-    '''
+    """
     return max(race.largest_contribution for race in races)
+
 
 @register.filter
 def percentage(obj, total):
-    '''
+    """
     Return a campaign's share (percentage) of the total funds raised in a given
     context.
-    '''
+    """
     return obj.share_of_funds(total)
+
 
 @register.filter
 def format_years(years):
-    '''
+    """
     Return a nicely formatted string from a list of years. For continuous years,
     display a range; for disjoint years, return a comma-separated list.
-    '''
+    """
+
     def get_ranges(current, future, final):
-        '''
+        """
         Given a list of years, return a list of lists corresponding to the years
         spanned, with continuous ranges represented by nested lists.
         e.g. `[2012, 2013, 2015]` will return `[[2012, 2013], [2015]]`.
-        '''
+        """
         # Base case
         if len(future) == 0:
             final.append(current)
@@ -129,12 +142,12 @@ def format_years(years):
         return get_ranges(current, future, final)
 
     def format_range(rng):
-        '''
+        """
         Given a list of years, return a string representing the range of years
         spanned. e.g. `[2012, 2013, 2014]` will return `2012-2014`.
-        '''
+        """
         if len(rng) == 0:
-            return ''
+            return ""
         elif len(rng) == 1:
             return rng[0]
         else:
@@ -142,20 +155,21 @@ def format_years(years):
             return "{start} - {end}".format(start=start, end=end)
 
     if len(years) == 0:
-        return ''
+        return ""
     elif len(years) == 1:
         return str(years[0])
     else:
         sorted_years = sorted(years)
         ranges = get_ranges([sorted_years[0]], sorted_years[1:], [])
-        return ', '.join(format_range(rng) for rng in ranges)
+        return ", ".join(format_range(rng) for rng in ranges)
+
 
 @register.simple_tag
 def lobbyist_contributions(obj, employer_id, short=False):
-    '''
+    """
     Return the total amount of political contributions by a lobbyist working
     under a given employer.
-    '''
+    """
     funds = obj.total_contributions(employer_id=employer_id)
 
     if short:
@@ -165,12 +179,13 @@ def lobbyist_contributions(obj, employer_id, short=False):
 
     return output
 
+
 @register.simple_tag
 def lobbyist_expenditures(obj, employer_id, short=False):
-    '''
+    """
     Return the total amount of expenditures by a lobbyist working under a
     given employer.
-    '''
+    """
     funds = obj.total_expenditures(employer_id=employer_id)
 
     if short:
