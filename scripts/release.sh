6bccdf94b69c1f01fdbf5df7bbe56ba859311ae9
Hannah Cushman Garland <hannah.cushman@datamade.us>
2023-12-01 11:03:19 -0600

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,8 @@ python manage.py migrate --noinput
 
 if [ `psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM camp_fin_candidate"` -eq "0" ]; then
     python manage.py import_data
-    python -W ignore manage.py import_data_2023
+    python manage.py import_api_data
+    python manage.py import_api_data --transaction-type EXP
     python manage.py make_search_index
 fi
 
