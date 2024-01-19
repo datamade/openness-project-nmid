86698d4114eda7d6e86ae645bc0331cb089f3149
Hannah Cushman Garland <hannah.cushman@datamade.us>
2024-01-19 10:58:47 -0600

    Remove lobbyist data from the site

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,6 @@ python manage.py createcachetable
 python manage.py migrate --noinput
 
 if [ `psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM camp_fin_candidate"` -eq "0" ]; then
-    python manage.py import_data
     make import/offices import/CON_2023 import/EXP_2023
     python manage.py make_search_index
 fi
