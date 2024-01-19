712f78bc51b20f3f0bad26ea3aa383fc1844206b
Hannah Cushman Garland <hannah.cushman@datamade.us>
2024-01-19 11:01:29 -0600

    Remove lobbyist URLs

--- nmid/urls.py
+++ nmid/urls.py
@@ -19,61 +19,115 @@ from django.conf.urls import handler404, handler500
 
 from rest_framework import routers
 
-from camp_fin.views import IndexView, CandidateList, CandidateDetail, \
-    LobbyistList, LobbyistDetail, LobbyistTransactionList, LobbyistPortal,\
-    ContributionViewSet, ExpenditureViewSet, ContributionDownloadViewSet, \
-    ExpenditureDownloadViewSet, TransactionViewSet, TopDonorsView, \
-    TopExpensesView, CommitteeList, CommitteeDetail, ContributionDetail, DownloadView, \
-    ExpenditureDetail, SearchView, SearchAPIView, DonationsView, \
-    LoanViewSet, TopEarnersView, TopEarnersWidgetView, AboutView, \
-    flush_cache, bulk_candidates, bulk_committees, bulk_lobbyists, bulk_employers, \
-    bulk_employments, OrganizationList, OrganizationDetail, LobbyistContributionViewSet, \
-    LobbyistExpenditureViewSet
+from camp_fin.views import (
+    IndexView,
+    CandidateList,
+    CandidateDetail,
+    LobbyistList,
+    LobbyistDetail,
+    LobbyistTransactionList,
+    LobbyistPortal,
+    ContributionViewSet,
+    ExpenditureViewSet,
+    ContributionDownloadViewSet,
+    ExpenditureDownloadViewSet,
+    TransactionViewSet,
+    TopDonorsView,
+    TopExpensesView,
+    CommitteeList,
+    CommitteeDetail,
+    ContributionDetail,
+    DownloadView,
+    ExpenditureDetail,
+    SearchView,
+    SearchAPIView,
+    DonationsView,
+    LoanViewSet,
+    TopEarnersView,
+    TopEarnersWidgetView,
+    AboutView,
+    flush_cache,
+    bulk_candidates,
+    bulk_committees,
+    bulk_lobbyists,
+    bulk_employers,
+    bulk_employments,
+    OrganizationList,
+    OrganizationDetail,
+    LobbyistContributionViewSet,
+    LobbyistExpenditureViewSet,
+)
 
 router = routers.DefaultRouter()
-router.register(r'contributions', ContributionViewSet, base_name='contributions')
-router.register(r'expenditures', ExpenditureViewSet, base_name='expenditures')
-router.register(r'bulk/contributions', ContributionDownloadViewSet, base_name='bulk-contributions')
-router.register(r'bulk/expenditures', ExpenditureDownloadViewSet, base_name='bulk-expenditures')
-router.register(r'transactions', TransactionViewSet, base_name='transactions')
-router.register(r'top-donors', TopDonorsView, base_name='top-donors')
-router.register(r'top-expenses', TopExpensesView, base_name='top-expenses')
-router.register(r'search', SearchAPIView, base_name='search')
-router.register(r'loans', LoanViewSet, base_name='loan')
-router.register(r'bulk/lobbyist-contributions', LobbyistContributionViewSet, base_name='bulk-lobbyist-contributions')
-router.register(r'bulk/lobbyist-expenditures', LobbyistExpenditureViewSet, base_name='bulk-lobbyist-expenditures')
+router.register(r"contributions", ContributionViewSet, base_name="contributions")
+router.register(r"expenditures", ExpenditureViewSet, base_name="expenditures")
+router.register(
+    r"bulk/contributions", ContributionDownloadViewSet, base_name="bulk-contributions"
+)
+router.register(
+    r"bulk/expenditures", ExpenditureDownloadViewSet, base_name="bulk-expenditures"
+)
+router.register(r"transactions", TransactionViewSet, base_name="transactions")
+router.register(r"top-donors", TopDonorsView, base_name="top-donors")
+router.register(r"top-expenses", TopExpensesView, base_name="top-expenses")
+router.register(r"search", SearchAPIView, base_name="search")
+router.register(r"loans", LoanViewSet, base_name="loan")
 
-handler404 = 'camp_fin.views.four_oh_four'
-handler500 = 'camp_fin.views.five_hundred'
+handler404 = "camp_fin.views.four_oh_four"
+handler500 = "camp_fin.views.five_hundred"
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^about/$', AboutView.as_view(), name='about'),
-    url(r'^donations/$', DonationsView.as_view(), name='donations'),
-    url(r'^search/$', SearchView.as_view(), name='search'),
-    url(r'^candidates/$', CandidateList.as_view(), name='candidate-list'),
-    url(r'^candidates/(?P<slug>[\w-]+)/$', CandidateDetail.as_view(), name='candidate-detail'),
-    url(r'^contributions/(?P<pk>[0-9]+)/$', ContributionDetail.as_view(), name='contribution-detail'),
-    url(r'^expenditures/(?P<pk>[0-9]+)/$', ExpenditureDetail.as_view(), name='expenditure-detail'),
-    url(r'^committees/$', CommitteeList.as_view(), name='committee-list'),
-    url(r'^committees/(?P<slug>[\w-]+)/$', CommitteeDetail.as_view(), name='committee-detail'),
-    url(r'^downloads/$', DownloadView.as_view(), name='downloads'),
-    url(r'^lobbyist-portal/$', LobbyistPortal.as_view(), name='lobbyist-portal'),
-    url(r'^lobbyists/$', LobbyistList.as_view(), name='lobbyist-list'),
-    url(r'^lobbyists/(?P<slug>[\w-]+)/$', LobbyistDetail.as_view(), name='lobbyist-detail'),
-    url(r'^organizations/$', OrganizationList.as_view(), name='organization-list'),
-    url(r'^organizations/(?P<slug>[\w-]+)/$', OrganizationDetail.as_view(), name='organization-detail'),
-    url(r'^transactions/$', LobbyistTransactionList.as_view(), name='lobbyist-transaction-list'),
-    url(r'^api/bulk/candidates/$', bulk_candidates, name='bulk-candidates'),
-    url(r'^api/bulk/committees/$', bulk_committees, name='bulk-committees'),
-    url(r'^api/bulk/lobbyists/$', bulk_lobbyists, name='bulk-lobbyists'),
-    url(r'^api/bulk/employers/$', bulk_employers, name='bulk-employers'),
-    url(r'^api/bulk/employments/$', bulk_employments, name='bulk-employments'),
-    url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-    url(r'^top-earners/$', TopEarnersView.as_view(), name='top-earners'),
-    url(r'^widgets/top-earners/$', TopEarnersWidgetView.as_view(), name='widget-top-earners'),
-    url(r'^flush-cache/$', flush_cache, name='flush-cache'),
+    url(r"^admin/", admin.site.urls),
+    url(r"^$", IndexView.as_view(), name="index"),
+    url(r"^about/$", AboutView.as_view(), name="about"),
+    url(r"^donations/$", DonationsView.as_view(), name="donations"),
+    url(r"^search/$", SearchView.as_view(), name="search"),
+    url(r"^candidates/$", CandidateList.as_view(), name="candidate-list"),
+    url(
+        r"^candidates/(?P<slug>[\w-]+)/$",
+        CandidateDetail.as_view(),
+        name="candidate-detail",
+    ),
+    url(
+        r"^contributions/(?P<pk>[0-9]+)/$",
+        ContributionDetail.as_view(),
+        name="contribution-detail",
+    ),
+    url(
+        r"^expenditures/(?P<pk>[0-9]+)/$",
+        ExpenditureDetail.as_view(),
+        name="expenditure-detail",
+    ),
+    url(r"^committees/$", CommitteeList.as_view(), name="committee-list"),
+    url(
+        r"^committees/(?P<slug>[\w-]+)/$",
+        CommitteeDetail.as_view(),
+        name="committee-detail",
+    ),
+    url(r"^downloads/$", DownloadView.as_view(), name="downloads"),
+    url(r"^organizations/$", OrganizationList.as_view(), name="organization-list"),
+    url(
+        r"^organizations/(?P<slug>[\w-]+)/$",
+        OrganizationDetail.as_view(),
+        name="organization-detail",
+    ),
+    url(
+        r"^transactions/$",
+        LobbyistTransactionList.as_view(),
+        name="lobbyist-transaction-list",
+    ),
+    url(r"^api/bulk/candidates/$", bulk_candidates, name="bulk-candidates"),
+    url(r"^api/bulk/committees/$", bulk_committees, name="bulk-committees"),
+    url(r"^api/bulk/employers/$", bulk_employers, name="bulk-employers"),
+    url(r"^api/bulk/employments/$", bulk_employments, name="bulk-employments"),
+    url(r"^api/", include(router.urls)),
+    url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    url(r"^ckeditor/", include("ckeditor_uploader.urls")),
+    url(r"^top-earners/$", TopEarnersView.as_view(), name="top-earners"),
+    url(
+        r"^widgets/top-earners/$",
+        TopEarnersWidgetView.as_view(),
+        name="widget-top-earners",
+    ),
+    url(r"^flush-cache/$", flush_cache, name="flush-cache"),
 ]
