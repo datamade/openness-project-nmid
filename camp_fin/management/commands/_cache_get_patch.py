4223b8d5ea20b407298e33719e9a7c03ef3cf69d
Forest Gregg <fgregg@datamade.us>
2024-01-29 13:49:56 -0500

    limit size of cache

--- camp_fin/management/commands/_cache_get_patch.py
+++ camp_fin/management/commands/_cache_get_patch.py
@@ -5,7 +5,7 @@ from django.db.models.query import QuerySet
 
 def cache_func(func):
     @functools.wraps(func)
-    @functools.lru_cache(maxsize=None)
+    @functools.lru_cache(maxsize=1024)
     def with_cache(self, *args, **kwargs):
         return func(self, *args, **kwargs)
 
