33d41003317e85e9f89d4890d966aae9d551a00b
Forest Gregg <fgregg@datamade.us>
2024-04-01 12:35:08 -0400

    build nightly data in release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-python manage.py createcachetable 
+python manage.py createcachetable
 python manage.py migrate --noinput
 
 if [ `psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM camp_fin_candidate"` -eq "0" ]; then
-    make import/offices import/CON_2023 import/EXP_2023
+    make nightly
     python manage.py make_search_index
 fi
 
