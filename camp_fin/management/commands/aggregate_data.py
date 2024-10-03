16dfa3ddfe19641e00a76c2461348c7bfb8752cd
Hannah Cushman Garland <hannah.cushman@datamade.us>
2024-10-03 12:02:54 -0500

    Update aggregate queries so that contributions and expenditures are available for a continuous interval

--- camp_fin/management/commands/aggregate_data.py
+++ camp_fin/management/commands/aggregate_data.py
@@ -38,6 +38,31 @@ class Command(BaseCommand):
                     )
                 )
 
+            continuous_interval_query = """
+                CREATE MATERIALIZED VIEW {transaction_type}_by_{interval} AS (
+                    WITH {interval}_summaries AS (
+                        {summary_query}
+                    )
+                    SELECT
+                        continuous_interval.entity_id,
+                        continuous_interval.{interval},
+                        COALESCE({interval}_summaries.amount, 0) AS amount
+                    FROM (
+                        SELECT DISTINCT
+                            entity_id,
+                            generate_series(
+                                MIN({interval}) OVER (PARTITION BY entity_id),
+                                MAX({interval}) OVER (PARTITION BY entity_id),
+                                '1 {interval}'
+                            ) AS {interval}
+                        FROM {interval}_summaries
+                    ) continuous_interval
+                    LEFT JOIN {interval}_summaries
+                    USING (entity_id, {interval})
+                    ORDER BY entity_id, {interval}
+                )
+            """
+
             try:
                 self.executeTransaction(
                     """
@@ -47,17 +72,16 @@ class Command(BaseCommand):
                     )
                 )
             except ProgrammingError:
-                view = """
-                    CREATE MATERIALIZED VIEW contributions_by_{0} AS (
-                      SELECT
+                summary_query = """
+                    SELECT
                         SUM(amount) AS amount,
                         entity_id,
-                        {0}
-                      FROM (
+                        {interval}
+                    FROM (
                         SELECT
                           SUM(t.amount) AS amount,
                           f.entity_id,
-                          MAX(date_trunc('{0}', t.received_date)) AS {0}
+                          MAX(date_trunc('{interval}', t.received_date)) AS {interval}
                         FROM camp_fin_transaction AS t
                         JOIN camp_fin_transactiontype AS tt
                           ON t.transaction_type_id = tt.id
@@ -68,21 +92,26 @@ class Command(BaseCommand):
                             'Monetary Contribution',
                             'Anonymous Contribution'
                           )
-                        GROUP BY f.entity_id, date_trunc('{0}', t.received_date)
+                        GROUP BY f.entity_id, date_trunc('{interval}', t.received_date)
                         UNION
                         SELECT
                           SUM(l.amount) AS amount,
                           f.entity_id,
-                          MAX(date_trunc('{0}', l.received_date)) AS {0}
+                          MAX(date_trunc('{interval}', l.received_date)) AS {interval}
                         FROM camp_fin_loan AS l
                         JOIN camp_fin_filing AS f
                           ON l.filing_id = f.id
-                        GROUP BY f.entity_id, date_trunc('{0}', l.received_date)
-                      ) AS s
-                      GROUP BY entity_id, {0}
-                    )
+                        GROUP BY f.entity_id, date_trunc('{interval}', l.received_date)
+                    ) contributions_and_loans
+                        GROUP BY entity_id, {interval}
                 """.format(
-                    interval
+                    interval=interval
+                )
+
+                view = continuous_interval_query.format(
+                    transaction_type="contributions",
+                    summary_query=summary_query,
+                    interval=interval,
                 )
 
                 self.executeTransaction(view)
@@ -96,17 +125,16 @@ class Command(BaseCommand):
                     )
                 )
             except ProgrammingError:
-                view = """
-                    CREATE MATERIALIZED VIEW expenditures_by_{0} AS (
+                summary_query = """
                       SELECT
                         entity_id,
                         SUM(amount) AS amount,
-                        {0}
+                        {interval}
                       FROM (
                         SELECT
                           filing.entity_id,
                           SUM(e.amount) AS amount,
-                          date_trunc('{0}', e.received_date) AS {0}
+                          date_trunc('{interval}', e.received_date) AS {interval}
                         FROM camp_fin_transaction AS e
                         JOIN camp_fin_transactiontype AS tt
                           ON e.transaction_type_id = tt.id
@@ -116,14 +144,14 @@ class Command(BaseCommand):
                           ON filing.filing_period_id = fp.id
                         WHERE tt.contribution = FALSE
                           AND filing.filed_date >= '2010-01-01'
-                        GROUP BY filing.entity_id, date_trunc('{0}', e.received_date)
+                        GROUP BY filing.entity_id, date_trunc('{interval}', e.received_date)
 
                         UNION
 
                         SELECT
                           filing.entity_id,
                           SUM(lt.amount) AS amount,
-                          date_trunc('{0}', lt.transaction_date) AS {0}
+                          date_trunc('{interval}', lt.transaction_date) AS {interval}
                         FROM camp_fin_loantransaction AS lt
                         JOIN camp_fin_loantransactiontype AS ltt
                           ON lt.transaction_type_id = ltt.id
@@ -133,12 +161,17 @@ class Command(BaseCommand):
                           ON filing.filing_period_id = fp.id
                         WHERE ltt.description = 'Payment'
                           AND filing.filed_date >= '2010-01-01'
-                        GROUP BY filing.entity_id, date_trunc('{0}', lt.transaction_date)
+                        GROUP BY filing.entity_id, date_trunc('{interval}', lt.transaction_date)
                       ) AS s
-                      GROUP BY entity_id, {0}
-                    )
+                      GROUP BY entity_id, {interval}
                 """.format(
-                    interval
+                    interval=interval
+                )
+
+                view = continuous_interval_query.format(
+                    transaction_type="expenditures",
+                    summary_query=summary_query,
+                    interval=interval,
                 )
 
                 self.executeTransaction(view)
